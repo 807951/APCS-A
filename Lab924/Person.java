9be8ea3af0fabb246c45ac6447d65a18f26687a2
807951 <54371368+807951@users.noreply.github.com>
2019-09-26 10:29:17 -0700

    class 926
    substring methods

--- Lab924/Person.java
+++ Lab924/Person.java
@@ -2,7 +2,7 @@
  * contains parseString method
  *
  * @author (Arin Gadre)
- * @version (923)
+ * @version (926)
  * 
  * I chose format three: "First Middle Last"
  */
@@ -13,22 +13,59 @@ public class Person
     private String lName = "";
     private String userName = "";
     private String[] fullNameArray;
+    private int commaIndex = 0;
+    private Person[] p = new Person[2];
     public Person(String fullName)
     {
-        userName = fullName;           
+        userName = fullName;
+        
     }
 
     public void parseArray(){
-        fullNameArray = userName.split(" "); 
+        fullNameArray = userName.split(" ");
+        commaIndex = (fullNameArray[0]).indexOf(","); 
         label:
-        if(fullNameArray.length > 3 || fullNameArray.length < 3){
-            System.out.println("Looks like you didn't enter in the correct format!");
+        if(fullNameArray.length < 2 || fullNameArray.length>3){
+            System.out.println("wrong format");
             break label;
         }
         else{
-            fName = fullNameArray[0];
-            mName = fullNameArray[1];
-            lName = fullNameArray[2]; 
+            if(fullNameArray.length == 3){
+                if(commaIndex > 0){
+                    fullNameArray[0] = fullNameArray[0].substring(0,commaIndex);
+                    fName = fullNameArray[1];
+                    mName = fullNameArray[2];
+                    lName = fullNameArray[0];
+                    for(int i = 0; i < fullNameArray.length; i++){
+                        System.out.print(fullNameArray[i]);
+                    }
+                }
+                else if(commaIndex == -1){
+                    fName = fullNameArray[0];
+                    mName = fullNameArray[1];
+                    lName = fullNameArray[2];
+                    for(int i = 0; i < fullNameArray.length; i++){
+                        System.out.println(fullNameArray[i]);
+                    }
+                }
+            }
+            else if(fullNameArray.length == 2){
+                if(commaIndex > 0){
+                    fullNameArray[0] = (fullNameArray[0]).substring(0,commaIndex);
+                    fName = fullNameArray[1];
+                    lName = fullNameArray[0];
+                    for(int i = 0; i < fullNameArray.length; i++){
+                        System.out.println(fullNameArray[i]);
+                    }
+                }
+                else if(commaIndex == -1){
+                    fName = fullNameArray[0];
+                    lName = fullNameArray[1];
+                    for(int i = 0; i < fullNameArray.length; i++){
+                        System.out.println(fullNameArray[i]);
+                    }
+                }
+            }
         }
     }
 
@@ -43,4 +80,5 @@ public class Person
     public String getMiddleName(){
         return mName;
     }
+
 }
