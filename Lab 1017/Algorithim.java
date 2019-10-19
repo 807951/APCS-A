318e37df20a89791c99f7fee7da15568293d0ac2
Arin Gadre <54371368+807951@users.noreply.github.com>
2019-10-18 21:00:00 -0700

    1018

--- Lab 1017/Algorithim.java
+++ Lab 1017/Algorithim.java
@@ -6,10 +6,14 @@
 import java.util.ArrayList;
 public class Algorithim
 {
-    private static ArrayList<Integer> nums = new ArrayList<Integer>();
     public static void main(String[] args){
-        loadArray(nums, 10);
-        System.out.println(nums);
+        ArrayList<Integer> ints = new ArrayList<Integer>();
+        System.out.println();
+        System.out.println("After bubble sort with ArrayList: "
+        + bubbleSortArrayList(ints));
+    }
+    public static ArrayList<Integer> bubbleSortArrayList(ArrayList<Integer> nums){
+        loadArrayList(nums, 10);
         for(int i = nums.size(); i > 0; i--){
             for(int j = 0; j < nums.size()-1; j++){
                 if(nums.get(j) > nums.get(j+1)){
@@ -19,12 +23,31 @@ public class Algorithim
                 }
             }
         }
-        System.out.println(nums);
+        return nums;
     }
-
-    public static void loadArray(ArrayList<Integer> nums, int numOfElements){
+    public static void loadArrayList(ArrayList<Integer> nums, int numOfElements){
         for(int i = 0; i < numOfElements; i++){
             nums.add((int)(Math.random() * 100)+1);
         }
+        System.out.println("Before bubble sort: " + nums);
+    }
+    public static ArrayList<Integer> bubbleSortArray(int[] nums){
+        loadArray(nums, 10);
+        for(int i = nums.size(); i > 0; i--){
+            for(int j = 0; j < nums.size()-1; j++){
+                if(nums.get(j) > nums.get(j+1)){
+                    int n = nums.get(j);
+                    nums.set(j, nums.get(j+1));
+                    nums.set(j+1, n);
+                }
+            }
+        }
+        return nums;
+    }
+    public static void loadArray(int[] nums, int numOfElements){
+        for(int i = 0; i < numOfElements; i++){
+            nums[0] = ((int)(Math.random() * 100)+1);
+        }
+        System.out.println("Before bubble sort: " + nums);
     }
 }
