9c0910e0bfe0141c4fd3279992282956b8e7f016
Arin Gadre <54371368+807951@users.noreply.github.com>
2019-10-18 21:01:57 -0700

    1018

--- Lab 1017/Algorithim.java
+++ Lab 1017/Algorithim.java
@@ -27,23 +27,23 @@ public class Algorithim
     }
     public static void loadArrayList(ArrayList<Integer> nums, int numOfElements){
         for(int i = 0; i < numOfElements; i++){
-            nums.add((int)(Math.random() * 100)+1);
+            nums.add((int)(Math.random() * 100)+1); 
         }
         System.out.println("Before bubble sort: " + nums);
     }
-    public static ArrayList<Integer> bubbleSortArray(int[] nums){
+    /*public static ArrayList<Integer> bubbleSortArray(int[] nums){
         loadArray(nums, 10);
-        for(int i = nums.size(); i > 0; i--){
-            for(int j = 0; j < nums.size()-1; j++){
+        for(int i = nums.length; i > 0; i--){
+            for(int j = 0; j < nums.length-1; j++){
                 if(nums.get(j) > nums.get(j+1)){
-                    int n = nums.get(j);
+                    int n = nums.ge[j]);
                     nums.set(j, nums.get(j+1));
                     nums.set(j+1, n);
                 }
             }
         }
         return nums;
-    }
+    }*/
     public static void loadArray(int[] nums, int numOfElements){
         for(int i = 0; i < numOfElements; i++){
             nums[0] = ((int)(Math.random() * 100)+1);
