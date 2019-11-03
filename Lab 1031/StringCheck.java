0821849e226548ca9ae94f55e2f17f080023f8ce
Arin Gadre <54371368+807951@users.noreply.github.com>
2019-11-02 17:28:31 -0700

    1102

--- Lab 1031/StringCheck.java
+++ Lab 1031/StringCheck.java
@@ -1,8 +1,10 @@
 /**
- * makes 2 word checker methods
+ * makes 2 word checker methods and runs them by asking for user input
  * @author (Arin Gadre)
- * @version (1031)
+ * @version (1102)
  */
+import java.util.Scanner;
+import java.util.Random;
 import java.util.ArrayList;
 public class StringCheck
 {
@@ -48,15 +50,44 @@ public class StringCheck
         }
     }
     public static void main(String[] args){
-        ArrayList<String> str1 = new ArrayList<String>();
-        str1.add("obama prism");
-        str1.add("bruh momento numero dos");
-        str1.add("joe mama");
-        String[] str2 = {"obama prism", "bruh momento numero dos", "joe mama"};
-        String key = "dos";
-        ArrayList<String> result1 = wordChecker(str2, key);
-        System.out.println(result1);
-        String[] result2 = wordChecker(str1, key);
-        printArray(result2);
+        Scanner sc = new Scanner(System.in); // creates Scanner object
+        Random ran = new Random(); 
+        /* I didn't know how to decide which method to use
+         * so I left it up to chance */
+        System.out.println("How many Strings would you like to enter? "); // asks user for how many strings they want to input
+        int numOfStrings = sc.nextInt(); // used to set the size of the array thats contains user inputs
+        // also used to fix how many times the for loops will run
+        sc.nextLine(); // fixes line error with scanner
+        int randomInt = ran.nextInt(2); // returns 0 or 1 
+        String[] userInputArray = new String[numOfStrings]; // array of user input strings which will be passed into wordchecker
+        ArrayList<String> userInputArrayList = new ArrayList<String>(); // arraylist of user input strings which will be passed into wordchecker
+        ArrayList<String> resultArrayList = new ArrayList<String>(); // result of word checker
+        String[] resultArray; // result of wordchecker
+        System.out.print('\u000C'); // clears terminal screen
+        if(randomInt == 0){
+            for(int i = 0; i < numOfStrings; i++){
+                System.out.println("Enter string: "); // asks user for a string 
+                userInputArray[i] = sc.nextLine().trim(); // adds string to the string array
+            }
+        }
+        else{
+            //System.out.println("Wordchecker that takes an arraylist and returns an array");
+            for(int i = 0; i < numOfStrings; i++){
+                System.out.println("Enter string: "); // asks user for a string
+                userInputArrayList.add(sc.nextLine().trim()); // adds string to the array list
+            }
+        }
+        System.out.print('\u000C'); // clears terminal screen
+        System.out.println("What keyword would you like to search for? "); // asks user for a keyword
+        if(randomInt == 0){
+            resultArrayList = wordChecker(userInputArray, sc.nextLine().trim()); // returns string in which the key exists
+            System.out.print('\u000C'); // clears terminal screen
+            System.out.println(resultArrayList); // prints the string in which the variable key exists 
+        }
+        else{
+            resultArray = wordChecker(userInputArrayList, sc.nextLine().trim()); // returns string in which they key exists
+            System.out.print('\u000C'); // clears terminal screen
+            printArray(resultArray); // prints the string in which the variable key exists 
+        }
     }
 }
