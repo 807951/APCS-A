2379c5375da69d7ed5ab04cf08215fda3379ae93
Arin Gadre <agadre@WS-ACADBUS.EDU>
2020-01-08 10:20:25 -0800

    1/8/2020

---
+++ Sem Two/Pumpkin Project/Pumpkin.java
@@ -0,0 +1,15 @@
+
+/**
+ * Write a description of class Pumpkin here.
+ *
+ * @author Arin Gadre
+ * @version 1-8-2020
+ */
+public class Pumpkin
+{
+    private int idNumber
+    public Pumpkin(int )
+    {
+
+    }
+}
---
+++ Sem Two/Student Tester Project/Student.java
@@ -0,0 +1,16 @@
+
+/**
+ * Write a description of class Student here.
+ *
+ * @author Arin Gadre
+ * @version 1-8-2020
+ */
+public class Student
+{
+    /*public Student()
+    {
+    }*/
+    public Student(String name, int age){
+        
+    }
+}
